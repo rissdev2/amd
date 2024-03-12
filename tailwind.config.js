e7d477b3447362c21c43bcef513032de24eca0f6
sulphitic786 <wqasimg@gmail.com>
2024-03-13 04:06:17 +0500

    blog detail page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@
 module.exports = {
     content: ["./build/*.html"],
     theme: {
+     
         extend: {
             backgroundImage: {
                 images: "url('./assets/images.png')",
             },
+            colors:{
+                'text-color':'#A4A4A4',    
+                'bg-black':'#000',    
+                'text-white':'#fff',    
+          },
         },
     },
     plugins: [],
 };
+
+
+
+
+  
